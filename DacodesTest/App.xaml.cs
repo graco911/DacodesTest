08e818846674a2d4139363f22fb659f3c099405d
Carlos Graniel <caco572@hotmail.com>
2020-06-05 21:15:05 -0500

    Agregar archivos de proyecto.

---
+++ DacodesTest/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using DacodesTest.Views;
+using Xamarin.Forms;
+
+namespace DacodesTest
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
